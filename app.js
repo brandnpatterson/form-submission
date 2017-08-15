82a7adfe05567b229ee3934f939f26b5cb13a797
Brandon Patterson <brandonpatterson@Brandons-MacBook-Pro.local>
2017-08-14 19:14:19 -0500

    added post req for node side

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const port = process.env.PORT || 8887;
+var bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req,res) => {
+   res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(process.env.PORT || port, () => {
