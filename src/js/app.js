aad459c8f44721c06cf1f582e42ef1ea97b4e0bd
Brandon Patterson <brandnpatterson@gmail.com>
2018-11-28 02:15:02 -0600

    error display

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,9 @@
 // const devURL = 'http://localhost:3000/api/v1/submissions';
 // const prodURL = 'https://form-component-api.herokuapp.com/api/v1/submissions';
 
-import './oregano';
+import 'coriander';
 
-const form = {
-  init() {
-    this.cacheDOM();
-    this.bindEvents();
-  },
-  cacheDOM() {
-    this.$form = document.querySelector('.form');
-  },
-  bindEvents() {
-    this.submitForm();
-  },
-  submitForm() {
-    this.$form.oregano();
-  }
-};
-
-form.init();
+var $form = document.querySelector('.form');
+$form.coriander({
+  onChange: true
+});
