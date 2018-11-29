9aa34c7d6deb85c51562ab6a5883936e280b0c87
Brandon Patterson <brandnpatterson@gmail.com>
2018-11-28 19:50:32 -0600

    complete form submission

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,52 @@
-// const devURL = 'http://localhost:3000/api/v1/submissions';
-// const prodURL = 'https://form-component-api.herokuapp.com/api/v1/submissions';
-
+import axios from 'axios';
 import 'coriander';
 
-var $form = document.querySelector('.form');
+const req = 'https://form-component-api.herokuapp.com/api/v1/submissions';
+
+const $form = document.querySelector('.form');
+const $buttonRequest = document.querySelector('.button-request');
+
 $form.coriander({
-  onChange: true
+  onChange: true,
+  onSubmit(data) {
+    const namesObj = {};
+
+    data.inputs.forEach(d => {
+      if (d.type === 'radio') {
+        namesObj[d.name] = d.nextElementSibling.textContent;
+      } else {
+        namesObj[d.name] = d.value;
+      }
+    });
+
+    axios.post(req, namesObj).then(() => {
+      data.inputs.forEach(input => {
+        input.value = '';
+        input.checked = false;
+      });
+    });
+  }
+});
+
+$buttonRequest.addEventListener('click', () => {
+  axios.get(req).then(req => {
+    const $catData = document.querySelector('#cat-data');
+    const $dogData = document.querySelector('#dog-data');
+
+    req.data.map(person => {
+      const catItem = document.createElement('li');
+      const dogItem = document.createElement('li');
+
+      catItem.innerHTML = `<li class="item-request">${person.name} said ${
+        person.cat
+      }!</li>`;
+
+      dogItem.innerHTML = `<li class="item-request">${person.name} said ${
+        person.dog
+      }!</li>`;
+
+      $catData.appendChild(catItem);
+      $dogData.appendChild(dogItem);
+    });
+  });
 });
